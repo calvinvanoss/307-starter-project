254b142a78d2afa715baa670f5535b0bcf80de32
calvinvanoss <cvanoss@calpoly.edu>
2023-10-01 21:24:26 -0700

    add submit button

--- react-frontend/src/Form.js
+++ react-frontend/src/Form.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 
 
 
-function Form() {
+function Form(props) {
   const [person, setPerson] = useState(
      {
         name: "",
@@ -21,6 +21,12 @@ function Form() {
          {name: value, job: person['job']}   
        );
   }
+
+  function submitForm() {
+    props.handleSubmit(person);
+    setPerson({name: '', job: ''});
+  }
+
   return (
     <form>
       <label htmlFor="name">Name</label>
@@ -37,6 +43,7 @@ function Form() {
         id="job"
         value={person.job}
         onChange={handleChange} />
+        <input type="button" value="Submit" onClick={submitForm} />
     </form>
 );
 
